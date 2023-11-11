6ced919d4555446265b1e47f57005315cd59ff4e
mjmorelos <markjoseph.morelos@edu.sait.ca>
2023-11-10 22:11:55 -0700

    working on #6 on doc

---
+++ src/list/MyStack.java
@@ -0,0 +1,37 @@
+package list;
+
+import utilities.StackADT;
+
+public class MyStack implements StackADT<T> {
+
+	@Override
+	public void push(T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
