a0655261ca0631d6f0866b5112eeacf7161b8c27
mjmorelos <markjoseph.morelos@edu.sait.ca>
2023-11-10 20:46:01 -0700

    added comments on stack & queue

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -1,9 +1,35 @@
 package utilities;
 
 public interface QueueADT<T> {
+    /**
+     * Adds one item to the rear of this queue.
+     * @param item to be added to the rear of this queue.
+     */
 	void enqueue(T item);
+    /**
+     * Removes and returns the front item of the queue.
+     * @return the item at the front of this queue.
+     * @throws EmptyCollectionException if the queue is empty.
+     */
     T dequeue();
+    /**
+     * Returns without removing the front item of the queue.
+     * @return the item at the front of this queue.
+     * @throws EmptyCollectionException if the queue is empty.
+     */
     T peek();
+    /**
+     * Returns true if this queue contains no items.
+     * @return true if this queue is empty, false otherwise.
+     */
     boolean isEmpty();
+    /**
+     * Returns the number of items in this queue.
+     * @return the number of items in this queue.
+     */
     int size();
+    /**
+     * Returns a string representation of this queue.
+     * @return a string representation of this queue.
+     */
 }
--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -1,9 +1,35 @@
 package utilities;
 
 public interface StackADT<T> {
+    /**
+     * Adds one item to the top of this stack.
+     * @param item the item to be added to the top of this stack.
+     */
 	void push(T item);
+    /**
+     * Removes and returns the top item from the stack
+     * @return the item at the top of this stack.
+     * @throws EmptyCollectionException if the stack is empty.
+     */
     T pop();
+    /**
+     * Returns without removing the top item of the stack.
+     * @return the item at the top of this stack.
+     * @throws EmptyCollectionException if the stack is empty.
+     */
     T peek();
+    /**
+     * Returns true if this stack contains no items.
+     * @return true if this stack is empty, false otherwise.
+     */
     boolean isEmpty();
+    /**
+     * Returns the number of items in this stack.
+     * @return the number of items in this stack.
+     */
     int size();
+    /**
+     * Returns a string representation of this stack.
+     * @return a string representation of this stack.
+     */
 }
